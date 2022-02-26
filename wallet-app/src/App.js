c9c10b88ee86a19cd11cc34dc527d8cd27f4b492
Dalena <mimidalena@gmail.com>
2022-02-25 18:58:22 -0800

    imports and return

--- wallet-app/src/App.js
+++ wallet-app/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import Web3 from "web3";
+import Web3Modal from "web3modal";
+import Portis from "@portis/web3";
+
+
+export default function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1>Web3modal</h1>
+          <p>No wallet connected. Connect wallet to show accounts and their ETH balances.</p>
+
+          <div id="prepare">
+            <button class="btn btn-primary" id="btn-connect">
+              Connect Wallet
+            </button>
+          </div>
+
+          <div id="connected" style="display:none">
+            <button class="btn btn-primary" id="btn-disconnect">
+              Disconnect Wallet
+            </button>
+          </div>
+
+          <hr></hr>
+
+          <div id="network">
+            <p>Connected Blockchain: <span id="network-name"></span></p>
+            <p>Selected Account: <span id="selected-account"></span></p>
+          </div>
+
+          <hr></hr>
+
+          <h3> All Account balances</h3>
+
+          <table class="table table-listing">
+            <thead>
+              <th>Address</th>
+              <th>ETH Balance</th>
+            </thead>
+
+            <tbody id="accounts"></tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
