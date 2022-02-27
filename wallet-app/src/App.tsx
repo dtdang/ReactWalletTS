ea18d76bc83f19752da2eda3bb51811d305cccfe
Dalena <mimidalena@gmail.com>
2022-02-26 18:05:54 -0800

    useStates

--- wallet-app/src/App.tsx
+++ wallet-app/src/App.tsx
@@ -1,26 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import './App.css';
+import Web3 from "web3";
+import Web3Modal from "web3modal";
+import Portis from "@portis/web3";
+import WalletConnectProvider from "@walletconnect/web3-provider";
 
-//import Web3 from "web3";
-//import Web3Modal from "web3modal";
-//import Portis from "@portis/web3";
+const providerOptions = {
+  walletconnect: {
+    package: WalletConnectProvider, // required
+    options: {
+      infuraId: "a83e54cc8ffd4887a0b8055d6130e765" // required
+    }
+  },
+  portis: {
+    package: Portis, // required
+    options: {
+      id: "a20d80e4-248b-4f8e-a547-d7e59e4fc6c6" // required
+    }
+  }
+};
+
+const web3Modal = new Web3Modal({
+  cacheProvider: false,
+  providerOptions, //required
+  disableInjectedProvider: false
+});
+
+interface InitialState {
+  address: string,
+  web3: any,
+  provider: any,
+  chainId: number,
+  networkId: number,
+  connected: boolean
+}
+
+function App(){
+  const [web3, setWeb3] = useState(null)
+  const [accounts, setAccounts] = useState(null)
+  const [chainId, setChainId] = useState(1)
+  const [networkId, setNetworkId] = useState(1)
+  const [account, setAccount] = useState(null)
+  const [provider, setProvider] = useState(null)
 
 
-export default function App(props) {
+
+  componentDidMount() {
+    if (web3Modal.cachedProvider){
+      web3Connect();
+    }
+  }
+
+  web3Connect = async () => {
+    const provider = await web3Modal.connect();
+    await provider.enable();
+
+    const accounts = await web3.eth.getAccounts();
+
+    const address = accounts[0];
+
+    const chainId = await web3.eth.chainId();
+
+    const networkId = await web3.eth.net.getId();
+  } 
   return (
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
           <h1>Web3modal</h1>
           <p>No wallet connected. Connect wallet to show accounts and their ETH balances.</p>
 
           <div id="prepare">
-            <button class="btn btn-primary" id="btn-connect">
+            <button className="btn btn-primary" id="btn-connect" onClick={() => showPrepare()}>
               Connect Wallet
             </button>
           </div>
 
           <div id="connected">
-            <button class="btn btn-primary" id="btn-disconnect">
+            <button className="btn btn-primary" id="btn-disconnect">
               Disconnect Wallet
             </button>
           </div>
@@ -36,7 +93,7 @@ export default function App(props) {
 
           <h3> All Account balances</h3>
 
-          <table class="table table-listing">
+          <table className="table table-listing">
             <thead>
               <th>Address</th>
               <th>ETH Balance</th>
@@ -50,3 +107,4 @@ export default function App(props) {
   );
 }
 
+export default App;
